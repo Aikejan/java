5cdf4f6b9469749ff9fccdaeba0418d90f6a7ddc
Aikezhan Akhmatova <aikezhanakhmatova@Aikezhans-MacBook-Pro.local>
2023-01-25 20:15:51 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,215 @@
+import java.lang.constant.Constable;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+     //   myNumber(1,2,3);
+
+
+
+//          //  TODO  TUURA EMES JOLU ///
+     //   String [] footbolPlaers= {"Ronaldo", "Messi"};
+
+        // TODO TUURA JOLU//
+//        method{...name: "Ronaldo"};
+//        method(foobolPlaers);
+
+//        int[] array = {1, 2, 3, 4, 5, 6, 7};
+//
+//        System.out.println(minNamber(array));
+
+
+//        for (int d = 1; d <= 6; d++) {
+//            for (int b = 6; b >= 1; b--) {
+//                System.out.print("*");
+//
+//            }
+//            System.out.println();
+//        }
+
+
+        // TODO 2 ЧИ  / ФОР / ТАПШЫРМА !n
+      /*  Scanner a = new Scanner(System.in);
+        int d = a.nextInt();
+        for (int b = 1; b <=a; b++) {
+            d*=
+        }*/
+
+
+// TODO БАЛАТЫ / FOR /  ТАПШЫРМА 6 ОЙ
+
+       /* for (int c = 0; c <= 9; c++) {
+            for (int a = 2; a<=c; a++) {
+                System.out.print("*");*/
+
+
+        //T  TODO FOR LOOP 6-Й УРОК;
+
+        /*for(int i = 1; i<=100; i++){
+
+            System.out.println(i);
+    }*/
+        //TODO FOR МЕНЕН ТАБЛИЦА УМНОЖЕНИЕ ///
+
+     /*   Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        for (int i = 1; i <=10 ; i++){
+            System.out.printf("%d*%d=%d\n",a,i,(a*i));
+        }
+
+        Scanner b = new Scanner(System.in);
+        int d = b.nextInt();
+        for (int i = 1; i <=10 ; i++) {
+            System.out.printf("%d*%d=%d\n",d,i,(d*i));
+        };/*/
+
+        // TODO ПРАКТИКА  FOR
+
+
+      /*  for (int a = 1; a <10 ; a++) {
+            System.out.println("ВЕДИТЕ ДВА ЧИСЛО");
+            Scanner scanner = new Scanner(System.in);
+            int b = scanner.nextInt();
+            int d = scanner.nextInt();
+            System.out.printf("%d*%d=%d\n", b, d , (b*d));
+
+            System.out.println("НАДО ЛИ ЗАВЕРШИТ ВЫПОЛНЕНИЕ");
+            int c = scanner.nextInt();
+            if(c==1) {
+                System.out.println("ЗАВЕРШЕНО");
+                break;
+            }*/
+     /*   for (int c = 1; c <10 ; c++) {
+            System.out.println("ВЕДИТЕ ДВА ЧИСЛО");
+            Scanner scanner = new Scanner(System.in);
+            int f = scanner.nextInt();
+            int d = scanner.nextInt();
+            System.out.printf("%d*%d=%d\n,",f,d,(f*d));
+
+            System.out.println("НАДО ЛИ ЗАВЕРШИТ ВЫПОЛНЕНИЕ");
+            int a = scanner.nextInt();
+            if(a == 1) {
+                System.out.println("ЗАВЕРШЕНО");
+                break;
+        }*/
+        // TODO TAPSHURMA FOR BALATY;
+////
+////        Scanner scanner= new Scanner(System.in);
+////
+////       int a = scanner.ne
+//       for (int c = 1; c <=6; c++) {
+//            for (int i = 3; i >=c ; i--) {
+//                System.out.print("*");
+//
+//            }
+
+//
+//      Scanner a = new Scanner(System.in);
+//    int b = a.nextInt();
+//    int d = a.nextInt();
+//    if(b>d || d<b){
+//        System.out.println("ff");
+//    } else if (b%2==0) {
+//        System.out.println("ttt");
+//    } else if (d<b*2) {
+//
+//        System.out.println("hh");
+
+
+        // Scanner scanner = new Scanner(System.in);
+//            double b = scanner.nextDouble();
+//        for (int i = 0; i <b;  i++) {
+//            if(i<b ||b>i){
+//
+//            }else if(true)
+//                System.out.println(Math.floor(b));
+//
+//
+//        }
+
+//        String ip = scanner.nextLine();
+//        String[] result = ip.split("\\.");
+//        System.out.println("0."+result[result.length-1]);
+//        int bun = Integer.parseInt(result[result.length-1]);
+//        System.out.println(bun);
+//
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int d = scanner.nextInt();
+//        int b = 0;
+//        while (a >=d) {
+//            b++;
+//            System.out.println(a+"*"+d+"="+(a*d));
+//
+//
+//        }
+
+
+//    }
+//
+//    static Constable minNamber(int[] array) {
+//
+//        for (int i = 0; i < array.length; i++) {
+//            Arrays.sort(array);
+//            return array[0];
+//
+//
+//        }
+//        return null;
+//
+//    }
+
+
+        //TODO  //  VARARGS  ///
+
+
+
+
+//    public static void method(int... numbers){
+//        for (int i = 0; i < numbers.length; i++) {
+//            System.out.println(numbers([i]));
+//
+
+//        static void function(String [] name){
+//            for (String n:name ) {
+//                System.out.println(n);{
+//
+//           }
+//    static void myNumber( int... numbers){
+//        int sum = 0;
+//        for (int i = 0; i < numbers.length; i++) {
+//            sum = sum+numbers[i];
+//
+//        }
+//        System.out.println(sum);
+
+
+}
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
